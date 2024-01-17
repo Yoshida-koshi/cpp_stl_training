eb59edc548aeb8780703ee3e1ec6803e32776ca4
Yoshida-koshi <koshi2278@outlook.jp>
2024-01-17 12:17:05 +0900

    create bubble sort program

---
+++ bubble_sort.cpp
@@ -0,0 +1,23 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+int n, a[10000];
+
+int main() {
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n - i; j++) {
+            if (a[j] > a[j + 1]) {
+                swap(a[j], a[j + 1]);
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << a[i] << " ";
+    }
+    printf("\n");
+}
